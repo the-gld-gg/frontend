266e49581c40be9f0d80d325b21d43e3581f9ae1
Sharath Nagaraj <sharath.nagaraj@cheproximity.com.au>
2019-12-22 15:55:43 +1100

    Update default layout

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import styles from "./Layout.module.css";
 
 const Layout = ({
     children,
 }) => {
     return (
-        <div>
-            <div>
-                Header
+        <div className={styles.layout}>
+            {/* Header */}
+            <div className={styles.components}>
+                {children}
             </div>
-            {children}
         </div>
     )
 }
