8f40e0d7717156003c4808e665a74bfc04bd118d
Sharath Nagaraj <sharath.nagaraj@cheproximity.com.au>
2019-12-22 15:54:31 +1100

    New theme colors and icons

--- src/configs/customTheme.js
+++ src/configs/customTheme.js
@@ -1,22 +1,40 @@
+import React from 'react'
 import { theme } from "@chakra-ui/core";
 
 // Let's say you want to add custom colors
 const customTheme = {
   ...theme,
+  fonts: {
+    ...theme.fonts,
+    body: "Questrial, sans-serif",
+    heading: "Alegreya, serif"
+  },
   colors: {
     ...theme.colors,
     brand: {
-      900: "#1a365d",
-      800: "#153e75",
-      700: "#2a69ac",
+      900: "#0A154A",
+      800: "#EE215B",
+      700: "#5A0001",
+      600: "#550527",
+      500: "#643173",
+      400: "#FF7E2E"
     },
   },
+  icons: {
+    ...theme.icons,
+    location: {
+      path: <path fill="currentColor" d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z" />,
+      viewBox: "0 0 24 24"
+    },
+    people: {
+      path: <path fill="currentColor" d="M16 11c1.66 0 2.99-1.34 2.99-3S17.66 5 16 5c-1.66 0-3 1.34-3 3s1.34 3 3 3zm-8 0c1.66 0 2.99-1.34 2.99-3S9.66 5 8 5C6.34 5 5 6.34 5 8s1.34 3 3 3zm0 2c-2.33 0-7 1.17-7 3.5V19h14v-2.5c0-2.33-4.67-3.5-7-3.5zm8 0c-.29 0-.62.02-.97.05 1.16.84 1.97 1.97 1.97 3.45V19h6v-2.5c0-2.33-4.67-3.5-7-3.5z" />,
+      viewBox: "0 0 24 24"
+    },
+    controller: {
+      path: <path fill="currentColor" d="M21 6H3c-1.1 0-2 .9-2 2v8c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm-10 7H8v3H6v-3H3v-2h3V8h2v3h3v2zm4.5 2c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5zm4-3c-.83 0-1.5-.67-1.5-1.5S18.67 9 19.5 9s1.5.67 1.5 1.5-.67 1.5-1.5 1.5z" />,
+      viewBox: "0 0 24 24"
+    }
+  }
 };
 
 export default customTheme;
-
-
-// red: #e53e3e
-
-
-// @media (min-width: 500px)
