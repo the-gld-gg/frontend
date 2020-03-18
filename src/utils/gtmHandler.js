c08f4e4f44ca529798bc56a0b884d1ae484f684e
Sharath <sharath.nagaraj@cheproximity.com.au>
2020-03-19 05:57:54 +1100

    GTM prop names

--- src/utils/gtmHandler.js
+++ src/utils/gtmHandler.js
@@ -11,8 +11,8 @@ const gtmHandler = ({
 
     const newData = {
       event: event || "reactjs.event",
-      category,
-      action,
+      pageCategory: category,
+      pageAction: action,
       ...additionalProps
     }
 
