9e0c114ab9056d73f3ee7d41cff0f7ee52b68566
Sharath <sharath.nagaraj@cheproximity.com.au>
2020-04-14 05:48:39 +1000

    Logout

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
 import {
   Box,
@@ -10,74 +10,75 @@ import {
   MenuList,
   MenuGroup,
   MenuItem,
-  MenuDivider
+  MenuDivider,
+  useToast
 } from "@chakra-ui/core"
 import styles from "./Header.module.css"
 
-class Header extends React.Component {
-  constructor(props) {
-    super(props)
+const Header = (props) => {
+  const [user, setUser] = useState(localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : null)
+  const [toastMessage, setToastMessage] = useState(undefined)
+  const toast = useToast()
 
-    this.state = {
-      user: false
+  useEffect(() => {
+    if (toastMessage) {
+      const { title, body } = toastMessage
+
+      toast({
+        title,
+        description: body,
+        status: 'success',
+        duration: 5000,
+        isClosable: true
+      })
     }
-  }
+  }, [toastMessage, toast])
 
-  componentDidMount() {
-    this.setState({
-      user: localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : null
-    })
-  }
-
-  logout = () => {
-    this.setState({
-      user: null
-    }, () => {
-      localStorage.removeItem("user")
-    })
-  }
-  
-  render () {
-    const { user } = this.state
-
-    return (
-      <Box bg="brand.900" className={styles.header}>
-        <Link to="/">
-          <Image src="logo.svg" alt="The GLD" />
-        </Link>
-        <Menu>
-          <MenuButton as={Button} variant="outline" variantColor="brand">
-            <Icon name="menu" size="32px" color="brand.800" />
-          </MenuButton>
-          <MenuList>
-            <MenuItem>
-              <Link to="/">Home</Link>
-            </MenuItem>
-            <MenuDivider />
-            {
-              user &&
-              user.email &&
-              user.api_token ?
-              <MenuGroup title="Account">
-                <MenuItem onClick={() => { this.logout() }}>
-                  Logout
-                </MenuItem>
-              </MenuGroup>
-              :
-              <MenuGroup title="Account">
-                <MenuItem>
-                  <Link to="/login">Login</Link>
-                </MenuItem>
-                <MenuItem>
-                  <Link to="/register">Sign up</Link>
-                </MenuItem>
-              </MenuGroup>
-            }
-          </MenuList>
-        </Menu>
-      </Box>
-    )
-  }
+  return (
+    <Box bg="brand.900" className={styles.header}>
+      <Link to="/">
+        <Image src="logo.svg" alt="The GLD" />
+      </Link>
+      <Menu>
+        <MenuButton as={Button} variant="outline" variantColor="brand">
+          <Icon name="menu" size="32px" color="brand.800" />
+        </MenuButton>
+        <MenuList>
+          <MenuItem>
+            <Link to="/">Home</Link>
+          </MenuItem>
+          <MenuDivider />
+          {
+            user &&
+            user.email &&
+            user.api_token ?
+            <MenuGroup title="Account">
+              <MenuItem
+                onClick={() => {
+                  setUser(null)
+                  localStorage.removeItem("user")
+                  setToastMessage({
+                    title: "Logout",
+                    body: "You've successfully logged out."
+                  })
+                }}>
+                Logout
+              </MenuItem>
+            </MenuGroup>
+            :
+            <MenuGroup title="Account">
+              <MenuItem>
+                <Link to="/login">Login</Link>
+              </MenuItem>
+              <MenuItem>
+                <Link to="/register">Sign up</Link>
+              </MenuItem>
+            </MenuGroup>
+          }
+        </MenuList>
+      </Menu>
+    </Box>
+  )
 }
 
 export default Header
